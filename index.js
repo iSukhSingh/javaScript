5094aff615c7dac3c7b2e4e877e2f207f722c3fa
sonu2943singh <>
2022-11-24 01:34:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("JavaScript 3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "sukh";
+const author = "sukh"
+// author = 5 // Throws an error because constant cannot be changed
+b = "changed sukh"
+const sukh = 0;
+let c = null
+let d = undefined
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
