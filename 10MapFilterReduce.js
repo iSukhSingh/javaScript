79ad772cca207c6f2d4c99b64ede75a17466d182
cali1204 <qwertyuiop.s1204@gmail.com>
2022-11-30 17:06:19 -0500

    alest with webpaeg

--- 10MapFilterReduce.js
+++ 10MapFilterReduce.js
@@ -1,4 +1,4 @@
-let arr = [45, 23, 21]
+let arr = [45, 23, 21,43]
 // Array map method
 let a = arr.map((value, index, array) => {
   // console.log(value, index, array)
@@ -9,7 +9,7 @@ let a = arr.map((value, index, array) => {
 // Array filter method
 let arr2 = [45, 23, 21, 0, 3, 5]
 let a2 = arr2.filter((a) => {
-  return a < 10
+  return a < 40 && a > 10 
 })
 // console.log(a2, arr2)
 
