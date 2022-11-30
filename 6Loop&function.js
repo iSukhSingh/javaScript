bf0334f0178f7161cdf7238707e765608a9cc69d
Sukh Singh <69435459+iSukhSingh@users.noreply.github.com>
2022-11-29 19:40:49 -0500

    added while loop

--- 6Loop&function.js
+++ 6Loop&function.js
@@ -20,6 +20,29 @@ let obj = {
   shiv: 23
 }
 
+// while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 10;
+do{
+  console.log(i)
+  i++;
+}while (i < n) 
+
+
+
 // For in loop
 for (let a in obj) {
   console.log("Marks of " + a + " are " + obj[a])
